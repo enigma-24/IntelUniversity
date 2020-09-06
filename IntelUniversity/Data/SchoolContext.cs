180a13acf4a77d62c4a79b848fd18de527dcd612
Aniket <aniket.d2402@outlook.com>
2020-09-05 20:26:11 -0400

    Added dbcontext class for application

---
+++ IntelUniversity/Data/SchoolContext.cs
@@ -0,0 +1,22 @@
+﻿using IntelUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntelUniversity.Data
+{
+    public class SchoolContext:DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options):base(options){}
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // code to override default behaviour of EF in naming tables as per dbset property names
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
