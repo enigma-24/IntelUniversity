2e4c61193a44a50a812f6ee860c612688b9eff0d
Aniket <aniket.d2402@outlook.com>
2020-09-13 23:29:34 -0400

    About Functionality added

--- IntelUniversity/Controllers/HomeController.cs
+++ IntelUniversity/Controllers/HomeController.cs
@@ -1,21 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using IntelUniversity.Data;
+using IntelUniversity.Models.SchoolViewModels;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using IntelUniversity.Models;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace IntelUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, SchoolContext schoolContext)
         {
             _logger = logger;
+            context = schoolContext;
         }
 
         public IActionResult Index()
@@ -23,9 +26,16 @@ namespace IntelUniversity.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public async Task<ActionResult> About()
         {
-            return View();
+            IQueryable<EnrollmentDateGroup> data = from student in context.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
