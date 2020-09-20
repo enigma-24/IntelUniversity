8924ee3f53ed170b695984cc164447c8c3b7026b
Aniket <aniket.d2402@outlook.com>
2020-09-19 20:45:40 -0400

    Added migrations

--- IntelUniversity/Migrations/20200919222816_Rowversion.Designer.cs
+++ IntelUniversity/Migrations/20200919222816_Rowversion.Designer.cs
@@ -4,14 +4,16 @@ using IntelUniversity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IntelUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20200919222816_Rowversion")]
+    partial class Rowversion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
